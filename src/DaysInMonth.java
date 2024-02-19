d96cb22b6c70f691728f35f3f952e4ade3f5c86e
dganessingh <danielleganessingh@outlook.com>
2024-02-18 19:05:53 -0500

    Second Commit

--- src/DaysInMonth.java
+++ src/DaysInMonth.java
@@ -9,7 +9,7 @@ public class DaysInMonth {
         System.out.println("Enter a year:");
         int year = scan.nextInt();
 
-        System.out.println("Enter a month (1-12):");
+        System.out.println("What month would you like to know the number of days it contains? Enter the month as a digit (1 to 12).");
         int month = scan.nextInt();
 
         YearMonth yearMonthObject = YearMonth.of(year, month);
